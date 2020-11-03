04d361ff12be83ef893d1a7878dc1b8f82933f9e
Jean Paul Magro <Jean.Magro.b42809@mcast.edu.mt>
2020-11-03 14:26:33 +0100

    First Migration to DB

---
+++ Solution1/ShoppingCart.Data/Context/ShoppingCartDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingCart.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCart.Data.Context
+{
+    public class ShoppingCartDbContext : DbContext
+    {
+
+        public ShoppingCartDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>().Property(x => x.Id).HasDefaultValueSql("NEWID()");
+
+        }
+
+    }
+}
