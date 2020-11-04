8a22c4cd1a93b3a755fbd4a05ad1b3547fd4907e
Jean Paul Magro <Jean.Magro.b42809@mcast.edu.mt>
2020-11-04 13:45:59 +0100

    Created IProductsService, Products Service and made the connection between Application Layer and Data Layer

---
+++ Solution1/ShoppingCart.Application/Interfaces/IProductsService.cs
@@ -0,0 +1,16 @@
+﻿using ShoppingCart.Application.ViewModels;
+using ShoppingCart.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShoppingCart.Application.Interfaces
+{
+    public interface IProductsService
+    {
+        IQueryable<ProductViewModel> GetProducts();
+
+        //void RateProduct(Guid id, string comment, double rating);
+    }
+}
---
+++ Solution1/ShoppingCart.Data/Repositories/ProductsRepository.cs
@@ -0,0 +1,43 @@
+﻿using ShoppingCart.Data.Context;
+using ShoppingCart.Domain.Interfaces;
+using ShoppingCart.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShoppingCart.Data.Repositories
+{
+    public class ProductsRepository : IProductsRepository
+    {
+        private ShoppingCartDbContext _context;
+
+        public ProductsRepository(ShoppingCartDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddProduct(Product p)
+        {
+            _context.Products.Add(p);
+            _context.SaveChanges();
+        }
+
+        public void DeleteProduct(Guid id)
+        {
+            var myProduct = GetProduct(id);
+            _context.Products.Remove(myProduct);
+            _context.SaveChanges();
+        }
+
+        public Product GetProduct(Guid id)
+        {
+            return _context.Products.SingleOrDefault(x => x.Id == id);
+        }
+
+        public IQueryable<Product> GetProducts()
+        {
+            return _context.Products;
+        }
+    }
+}
